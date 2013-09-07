b674db6f9f7bf9eb82bf2d185f307de939e84db6
Kedar Amladi <kamladi@gmail.com>
2013-09-07 05:39:22 -0400

    layout changes, added Template structure

---
+++ src/com/pennappsf13/cmu/TextBack/Template.java
@@ -0,0 +1,44 @@
+package com.pennappsf13.cmu.TextBack;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kamladi
+ * Date: 9/7/13
+ * Time: 4:36 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Template {
+    private String title;
+    private String text;
+    private Boolean isSelected;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void Template(String title, String text, Boolean isSelected) {
+        this.setTitle(title);
+        this.setText(text);
+        this.setSelected(isSelected);
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Boolean getSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(Boolean selected) {
+        isSelected = selected;
+    }
+}
